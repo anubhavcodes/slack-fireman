bac8859d6c1d4fb2e5d6099b43dfa417b1e87933
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-07-27 12:55:07 +0530

    Implementing oauth

--- src/backend/app/routes/oauth.js
+++ src/backend/app/routes/oauth.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import request from 'request';
 import 'dotenv/config';
 
 const router = Router();
@@ -8,7 +9,20 @@ router.get('/', (req, res) => {
   if (!code) {
     res.status(401).send('Unauthenticated');
   }
-  res.json({ status: 'working' });
+  const form = {
+    client_id: process.env.SLACK_CLIENT_ID,
+    client_secret: process.env.SLACK_CLIENT_SECRET,
+    code,
+  };
+  request.post('https://slack.com/api/oauth.access', { form }, (err, response, body) => {
+    console.log(body);
+    if (!err && response.statusCode === 200) {
+      console.log(JSON.parse(body).team_id, JSON.parse(body).access_token);
+      res.sendStatus(200);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 export default router;
