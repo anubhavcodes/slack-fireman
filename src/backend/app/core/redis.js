7c4e14ba149063a075ef53b0150870cde8ddf12d
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-07-27 11:51:26 +0530

    Adding redis

---
+++ src/backend/app/core/redis.js
@@ -0,0 +1,20 @@
+import redis from 'redis';
+import { promisify } from 'util';
+import 'dotenv/config';
+
+const client = redis.createClient({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
+});
+
+client.on('connect', () => {
+  console.log('connected to redis');
+});
+
+client.on('error', (err) => {
+  console.error(`Error starting redis - ${err}`);
+});
+
+export const getAsync = promisify(client.get).bind(client);
+
+export default client;
